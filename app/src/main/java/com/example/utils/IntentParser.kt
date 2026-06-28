7658901365cfc2c5226da6e252a381d2278496e3
j3susangar1ca <j3susangar1ca@gmail.com>
2026-06-28 12:55:46 -0600

    4

---
+++ app/src/main/java/com/example/utils/IntentParser.kt
@@ -0,0 +1,8 @@
+package com.example.utils
+
+object IntentParser {
+    fun parseYoutubeUrl(sharedText: String?): String? {
+        // YouTube URL extraction and validation logic will be implemented here.
+        return null
+    }
+}
