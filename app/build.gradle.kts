1598f38ca59abd8b5793be4db2cc76ea877defd3
j3susangar1ca <j3susangar1ca@gmail.com>
2026-06-29 02:26:15 -0600

    Cambios

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -116,7 +116,7 @@ dependencies {
   implementation(libs.okhttp)
   // implementation(libs.play.services.location)
   implementation(libs.retrofit)
-  implementation("com.arthenica:ffmpeg-kit-min:6.0-2.LTS")
+  implementation("com.arthenica:ffmpeg-kit-min:6.0")
   testImplementation(libs.androidx.compose.ui.test.junit4)
   testImplementation(libs.androidx.core)
   testImplementation(libs.androidx.junit)
