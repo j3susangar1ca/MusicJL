9b427c590dde5f3ec2784e976f174a9a26158fa4
j3susangar1ca <j3susangar1ca@gmail.com>
2026-06-28 19:28:53 -0600

    Cambios

--- app/src/main/java/com/example/domain/models/TrackInfo.kt
+++ app/src/main/java/com/example/domain/models/TrackInfo.kt
@@ -9,10 +9,11 @@ import com.squareup.moshi.JsonClass
 @JsonClass(generateAdapter = true)
 data class TrackInfo(
     @Json(name = "video_id") val videoId: String,
-    @Json(name = "title_clean") val titleClean: String,
+    val title: String,
     val artist: String,
     val album: String?,
     @Json(name = "cover_art_url") val coverArtUrl: String?,
+    @Json(name = "download_url") val downloadUrl: String,
     val bpm: Int?,
     @Json(name = "camelot_key") val camelotKey: String?
 )
